351230d0023b6c558d9b2897cb75db7ace62594f
Marianny Garcia <mdvgarciar@gmail.com>
2021-05-25 00:04:53 -0400

    firstcommit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-import validator from './validator.js';
+// only numbers
+function valideKey(event){
+	var code = (evt.which) ? evt.which : evt.keyCode;
+    
+    if(code==8) { // backspace.
+      return true;
+    } else if(code>=48 && code<=57) { // is a number.
+      return true;
+    } else{ // other keys.
+      return false;
+    }
+}
 
-console.log(validator);
+//Manipulación de pantallas para mostrar y ocultar
+
+function goToPage2() {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+  document.getElementById("p2").innerText = "Bienvenid@ " + document.getElementById("name_user").value;
+}
+
+function goToPage3() {
+  document.getElementById("page2").style.display = "none";
+  document.getElementById("page3").style.display = "block";
+}
+
+function goToPage4() {
+  document.getElementById("page3").style.display = "none";
+  document.getElementById("page4").style.display = "block";
+  document.getElementById("msgValid").innerText = "Tarjeta añadida con éxito";
+
+}
+
+function goToPage5() {
+  document.getElementById("page4").style.display = "none";
+  document.getElementById("page5").style.display = "block";
+  document.getElementById("typeCard").innerText = type(document.getElementById("cardValid").value);
+  document.getElementById("finalValue").innerText = maskify(document.getElementById("cardValid").value);
+}
+
+//Lógica de pantallas
+function luhn() {
+  /////////////////////////////redundancia con intencionalidad
+  if (isValid(document.getElementById("cardValid").value) == true) {
+    document.getElementById("p3").innerText = "Tarjeta válida.";
+    setTimeout(goToPage4, 2000);
+  } else {
+    document.getElementById("p3").innerText = "Tarjeta no válida";
+  }
+}
+
+
+/*import validator from './validator.js';
+
+console.log(validator);*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,52 @@
-const validator = {
-  // ...
-};
+// const validator = {};
+// export default validator;
 
-export default validator;
+// Función operacional del algoritmo de Luhn
+function isValid(creditCardNumber) {
+  var leng = creditCardNumber.length
+  var parity = leng % 2
+  var sum = 0
+  for (var i = leng - 1; i >= 0; i--) {
+    var d = parseInt(creditCardNumber.charAt(i))
+    if (i % 2 == parity) {
+      d *= 2
+    }
+    if (d > 9) {
+      d -= 9
+    }
+    sum += d
+  }
+  return (sum % 10) == 0
+  }
+  
+//Función Maskify arreglo caracteres
+function maskify(creditCardNumber) {
+  var leng = creditCardNumber.length
+  var hide = leng - 4;
+  var num = creditCardNumber.substring(hide, leng);
+  var string = "";
+  var i = 0;
+  while (i < hide) {
+    string += "#";
+    i++;
+  }
+  return string + num;
+}
+
+//Función determina si es MASTER / VISA / AMEX / DISCOVER
+function type(creditCardNumber) {
+  var num = parseInt(creditCardNumber.substring(0, 1));
+
+  switch (num) {
+    case 3:
+      return "AMEX";
+    case 4:
+      return "VISA";
+    case 5:
+      return "MASTERCARD";
+    case 6:
+      return "DISCOVER";
+    default:
+      return "DESCONOCIDO";
+  }
+}
